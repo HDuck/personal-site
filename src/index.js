163a10dcb638b4d2161070240301470c856f5385
HDuck <1ntranc3d@gmail.com>
2018-08-25 19:08:20 +0300

    Delete "Hello, webpack" theme

--- src/index.js
+++ src/index.js
@@ -1,11 +1,1 @@
 import "./sass/main.sass";
-
-function component() {
-  let element = document.createElement('div');
-
-  element.innerHTML = 'Hello, webpack!';
-
-  return element;
-}
-
-document.body.appendChild(component());
